a4fa88c58fcd915dce741167e2a3e68f59e9c7fa
mireqdev <33450987+mireqdev@users.noreply.github.com>
2017-11-09 22:42:45 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -9,25 +9,22 @@ class LegalAid():
     case_number = None
     
     def identify(self):
-        id = input("Are you the applicant or the respondant?")
-    
-    def relationship(self):
-        relationto = int(input("What is your relationship to the child or children in question?\n 1.Mother \n 2.Father \n 3.Other"))
-        if relationto == '3':
-            print("Please specify below")
-            relation_other = input(">>")
-        other_party = input("Is the other party your former partner? (y/n):")
-        if other_party == 'y':
-            relation_start = input("When did the relationship begin?(dd/mm/yyyy)\n>>")
-            courtship_date = input("Date of marriage or civil partnership if applicable(dd/mm/yyyy):\n")
-            relation_end = input("When did the relationship end?(dd/mm/yyyy)\n>>")
-        case_number = input("What is the case number stated on the court papers that you have?:\n")
+        id = int(input("Are you 1.the applicant or 2.the respondant?\n"))
+        if id == 1:
+            relationto = int(input("What is your relationship to the child or children in question?\n 1.Mother \n 2.Father \n 3.Other\n>>"))
+            if relationto == 3:
+                print("Please specify below\n")
+                relation_other = input(">>")
+            other_party = input("Is the other party your former partner? (y/n):")
+            if other_party == 'y':
+                relation_start = input("When did the relationship begin?(dd/mm/yyyy)\n>>")
+                courtship_date = input("Date of marriage or civil partnership if applicable(dd/mm/yyyy):\n")
+                relation_end = input("When did the relationship end?(dd/mm/yyyy)\n>>")
+            case_number = input("What is the case number stated on the court papers that you have?:\n")
 
 def main():
     legal = LegalAid()
     legal.identify()
-    if id == 'applicant':
-        legal.relationship()
 
       
 if __name__ == "__main__":
